8398d3b52220dce4308af26b7e3f422b62bb1753
JleseelleurQUB <josephleseelleur@gmail.com>
2023-10-19 18:01:14 +0100

    Update 3
    - lesson 4.3
    
    - added powerup entity, destroyed when picked up and sets powerup bool on player
    
    - added impulse force applied to enemies on contact while powerup is active, coroutine creates timer for powerup expiry
    
    - added powerup indicator that is set to active while powerup is applied to player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,17 +8,54 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRB;
     private GameObject focalPoint;
     public float speed;
+    public bool hasPowerup;
+    private float powerUpStrength = 15.0f;
+    public GameObject powerupIndicator;
+    private Vector3 offset;
+
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
         focalPoint = GameObject.Find("Focal Point");
+        offset = powerupIndicator.transform.position - transform.position;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        powerupIndicator.transform.position = transform.position + offset;
         float forwardInput = Input.GetAxis("Vertical");
         playerRB.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+            powerupIndicator.SetActive(true);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRB = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+
+            enemyRB.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
+            Debug.Log("Collided with " + collision.gameObject.name + " while affected by powerup");
+        }
     }
 }
