b37e34dfea4f6b398d24bbb6e934a0496bc194c6
JleseelleurQUB <josephleseelleur@gmail.com>
2023-10-23 20:31:04 +0100

    Update 4
    - Enemy wave implementation

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,21 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
     private float spawnRange = 9.0f;
+    public int enemyCount;
+    public int waveNumber = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, GenerateSpawnPos(), enemyPrefab.transform.rotation);
+ 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+
+        if (enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+        }
     }
 
     private Vector3 GenerateSpawnPos()
@@ -25,5 +36,15 @@ public class SpawnManager : MonoBehaviour
         Vector3 spawnPos = new Vector3(randomPosX, 0, randomPosZ);
 
         return spawnPos;
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        Instantiate(powerupPrefab, GenerateSpawnPos(), powerupPrefab.transform.rotation);
+
+        for (int i=0; i<enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPos(), enemyPrefab.transform.rotation);
+        }
     }
 }
